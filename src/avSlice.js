89601eee2a59dbc2dfe21e6626321d59a02c2e0e
Jasmine M <jasmine.moe957@gmail.com>
2026-06-17 06:14:51 -0400

    add for av and meals

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,44 +4,50 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-      img: "https://pixabay.com/photos/business-computer-conference-20031/",
-      name: "Projector",
-      cost: 3500,
-      quantity: 0,
+		img: "https://pixabay.com/images/download/business-20031_640.jpg",
+		name: "Projectors",
+		cost: 200,
+		quantity: 0,
+	},
+    {
+		img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+		name: "Speaker",
+		cost: 35,
+		quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/speakers-bluetooth-tech-speaker-4109274/",
-      name: "Speakers",
-      cost: 3500,
-      quantity: 0,
+		img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+		name: "Microphones",
+		cost: 45,
+		quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/public-speaking-mic-microphone-3926344/",
-      name: "Microphone",
-      cost: 3500,
-      quantity: 0,
+		img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+		name: "Whiteboards",
+		cost: 80,
+		quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/whiteboard-dry-erase-marker-blank-2903269/",
-      name: "Whiteboard",
-      cost: 3500,
-      quantity: 0,
+		img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+		name: "Signage",
+		cost: 80,
+		quantity: 0,
     },
-    {
-      img: "https://pixabay.com/photos/signpost-waypoint-wood-grain-board-235079/",
-      name: "Signs",
-      cost: 3500,
-      quantity: 0,
-    }
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
